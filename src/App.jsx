5ef430603b30af49f924db11d519d57071ed14fb
Ryan Bharath <89420193+Friendlygeckos@users.noreply.github.com>
2023-05-24 18:40:17 -0400

    feat: adding a weather to favorites will show up in the favorites page (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Weather from './feature/weather/Weather'
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="container">
       <div className="d-flex border-bottom pt-2 pb-2 mb-5">
         <div className="p-2 flex-grow-1">Redux Store</div>
+        <Navbar />
       </div>
       <Weather />
     </div>
