b3680b8e89356f81869ef897c3ffd59c08108d74
Ryan Bharath <dbossman60@gmail.com>
2023-05-27 00:31:09 -0400

    chore: stying the card component for the forecast cards

--- src/feature/forecast/ForecastCard.jsx
+++ src/feature/forecast/ForecastCard.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Col, Card } from 'react-bootstrap';
+import { Col, Card, Container, Row } from 'react-bootstrap';
 
 const ForecastCard = ({ forecast }) => {
   return (
-    <Card>
-      <Col xs={9} md={8} lg={6}>
-        <div className="card-body">
-          <h5 className="card-title">Temperature: {forecast.main.temp}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Feels Like: {forecast.main.feels_like}</h6>
-          <p className="card-text">Weather: {forecast.weather[0].main}</p>
-          <p className="card-text">Description: {forecast.weather[0].description}</p>
-        </div>
-      </Col>
-    </Card>
+    <Container style={{ width: '18rem' , height: '30rem'}}>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img
+                variant="top"
+                src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
+              />
+              <Card.Title>Temperature: {forecast.main.temp}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Feels Like: {forecast.main.feels_like}
+              </Card.Subtitle>
+              <Card.Text>Weather: {forecast.weather[0].main}</Card.Text>
+              <Card.Text>Description: {forecast.weather[0].description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
