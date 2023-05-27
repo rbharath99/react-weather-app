309ec149d5f6afa4eb7d0d3f3d5b9aa335d6b544
Ryan Bharath <dbossman60@gmail.com>
2023-05-27 00:48:23 -0400

    chore: styling the search bar component

--- src/feature/weather/Search.jsx
+++ src/feature/weather/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { Form, Button, InputGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux'
 import { fetchWeather } from './WeatherSlice';
 import { fetchForecast } from '../forecast/ForecastSlice';
@@ -16,19 +16,18 @@ function Search() {
     };
     return (
         <Form onSubmit={handleSubmit}>
-            <Row className="align-items-center">
-                <Col xs={9} md={8} lg={6}>
-                    <Form.Control
-                        type="text"
-                        value={city}
-                        onChange={(event) => setCity(event.target.value)}
-                        placeholder="Enter city name"
-                    />
-                </Col>
-                <Col xs={3} md={4} lg={6}>
-                    <Button type="submit" disabled={!city} variant="primary">Search</Button>
-                </Col>
-            </Row>
+            <InputGroup className="mb-3">
+                <Form.Control
+                    placeholder="Enter City Name"
+                    aria-label="Search for Weather"
+                    type="text"
+                    value={city}
+                    onChange={(event) => setCity(event.target.value)}
+                />
+                <Button type="submit" disabled={!city} variant="primary" >
+                    Search
+                </Button>
+            </InputGroup>
         </Form>
     );
 }
