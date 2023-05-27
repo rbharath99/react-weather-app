f6644e19776abed73f0f3c8d1e1aae9b308dc1e1
Ryan Bharath <dbossman60@gmail.com>
2023-05-27 01:03:35 -0400

    style: stylig navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import Weather from './feature/weather/Weather'
 import Navbar from './Navbar';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="container">
-      <div className="d-flex border-bottom pt-2 pb-2 mb-5">
-        <div className="p-2 flex-grow-1">Weather Forecast</div>
-        <Navbar />
-      </div>
+    <Container>
+      <Row className="border-bottom pt-2 pb-2 mb-5">
+        <Col>
+          <Navbar />
+        </Col>
+      </Row>
       <Weather />
-    </div>
+    </Container>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,6 +5,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 const NavigationBar = () => {
     return (
         <Navbar bg="light" expand="md">
+            <Navbar.Brand>Weather App</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/favorites">Favorites</Nav.Link>
