4b91402bbc67a4f9d6e42978bf8a053af6414609
Ryan Bharath <dbossman60@gmail.com>
2023-05-28 16:30:50 -0400

    feat: show data labels above the point

--- src/feature/graph/Graph.jsx
+++ src/feature/graph/Graph.jsx
@@ -9,7 +9,8 @@ import {
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 function Graph() {
 
@@ -22,46 +23,69 @@ function Graph() {
         LineElement,
         Title,
         Tooltip,
-        Legend
+        Legend,
+        ChartDataLabels
     );
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Temperature forecast',
-            },
-        },
-    };
-
-    let y = []
-    let x = []
     if (forecastData) {
-        for (let i = 0; i < forecastData.list.length; i++) {
-            y.push(forecastData.list[i].main.temp)
-            x.push(forecastData.list[i].dt_txt)
+        const temperatures = forecastData.list.map(item => item.main.temp);
+        const dates = forecastData.list.map(item => item.dt_txt);
+
+        const options = {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Temperature forecast',
+                },
+                datalabels: {
+                    anchor: 'end',
+                    align: 'top',
+                    color: 'black',
+                    font: {
+                        weight: 'bold',
+                    },
+                    formatter: (value) => value + '°C',
+                },
+            },
+            scales: {
+                x: {
+                    beginAtZero: false,
+                    title: {
+                        display: true,
+                        text: 'Date and Time',
+                    },
+                },
+                y: {
+                    beginAtZero: false,
+                    title: {
+                        display: true,
+                        text: 'Temperature (°C)',
+                    },
+                },
+            },
+        };
+
+        const data = {
+            labels: dates,
+            datasets: [
+                {
+                    label: 'Temperature',
+                    data: temperatures,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                }
+            ]
         }
+
+        return (
+            <Line options={options} data={data} />
+        );
     }
 
-    const data = {
-        labels: x,
-        datasets: [
-            {
-                label: '6-hour temperature forecast',
-                data: y,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            }
-        ]
-    }
-
-    return (
-        <Line options={options} data={data} />
-    );
 }
 
 export default Graph;
