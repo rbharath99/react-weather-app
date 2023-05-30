76a3bda49369ebdaf09c0e2e7dd80628a50e52bc
Ryan Bharath <dbossman60@gmail.com>
2023-05-30 00:07:18 -0400

    fix: navigation bar appears on both pages

---
+++ src/NavbarWrapper.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import { Row, Col } from 'react-bootstrap';
+
+function NavbarWrapper() {
+    return (
+        <Row className="border-bottom pt-2 pb-2 mb-5">
+            <Col>
+                <Navbar />
+                <Outlet />
+            </Col>
+        </Row>
+    );
+}
+
+export default NavbarWrapper;
