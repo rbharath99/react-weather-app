70fdb418d3223b658c24b5db120c49a8ed3416e1
Ryan Bharath <89420193+Friendlygeckos@users.noreply.github.com>
2023-05-30 00:23:55 -0400

    fix: navbar shows a dropdown for smaller screen sizes (#7)

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const NavigationBar = () => {
     return (
         <Navbar bg="light" expand="md">
             <Navbar.Brand>Weather App</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/favorites">Favorites</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/favorites">Favorites</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
